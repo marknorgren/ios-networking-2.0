60dca88e2e62fd93892ee87c3063ccb554c2fd7a
Jarrod Parkes <jarrod@udacity.com>
2015-09-10 13:40:14 -0500

    step 3.11

--- MyFavoriteMovies/MyFavoriteMovies/MovieDetailViewController.swift
+++ MyFavoriteMovies/MyFavoriteMovies/MovieDetailViewController.swift
@@ -40,9 +40,7 @@ class MovieDetailViewController: UIViewController {
     override func viewWillAppear(animated: Bool) {
         
         super.viewWillAppear(animated)
-        
-        print("viewWillAppear (MovieDetailViewController): implement me!")
-        
+                
         if let movie = movie {
             
             /* Setting some defaults ... */
@@ -52,12 +50,91 @@ class MovieDetailViewController: UIViewController {
             
             /* TASK A: Get favorite movies, then update the favorite buttons */
             /* 1A. Set the parameters */
+            let methodParameters = [
+                "api_key": appDelegate.apiKey,
+                "session_id": appDelegate.sessionID!
+            ]
+            
             /* 2A. Build the URL */
+            let urlString = appDelegate.baseURLSecureString + "account/\(appDelegate.userID!)/favorite/movies" + appDelegate.escapedParameters(methodParameters)
+            let url = NSURL(string: urlString)!
+            
             /* 3A. Configure the request */
+            let request = NSMutableURLRequest(URL: url)
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
             /* 4A. Make the request */
-            /* 5A. Parse the data */
-            /* 6A. Use the data! */
+            let task = session.dataTaskWithRequest(request) { (data, response, error) in
+                
+                /* GUARD: Was there an error? */
+                guard (error == nil) else {
+                    print("There was an error with your request: \(error)")
+                    return
+                }
+                
+                /* GUARD: Did we get a successful 2XX response? */
+                guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                    if let response = response as? NSHTTPURLResponse {
+                        print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                    } else if let response = response {
+                        print("Your request returned an invalid response! Response: \(response)!")
+                    } else {
+                        print("Your request returned an invalid response!")
+                    }
+                    return
+                }
+                
+                /* GUARD: Was there any data returned? */
+                guard let data = data else {
+                    print("No data was returned by the request!")
+                    return
+                }
+                
+                /* 5A. Parse the data */
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+                } catch {
+                    parsedResult = nil
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+                
+                /* GUARD: Did TheMovieDB return an error? */
+                guard (parsedResult.objectForKey("status_code") == nil) else {
+                    print("TheMovieDB returned an error. See the status_code and status_message in \(parsedResult)")
+                    return
+                }
+                
+                /* GUARD: Is the "results" key in parsedResult? */
+                guard let results = parsedResult["results"] as? [[String : AnyObject]] else {
+                    print("Cannot find key 'results' in \(parsedResult)")
+                    return
+                }
+                
+                /* 6A. Use the data! */
+                var isFavorite = false
+                let movies = Movie.moviesFromResults(results)
+                
+                for movie in movies {
+                    if movie.id == self.movie!.id {
+                        isFavorite = true
+                    }
+                }
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    if isFavorite {
+                        self.favoriteButton.hidden = true
+                        self.unFavoriteButton.hidden = false
+                    } else {
+                        self.favoriteButton.hidden = false
+                        self.unFavoriteButton.hidden = true
+                    }
+                }
+            }
+            
             /* 7A. Start the request */
+            task.resume()
         
             /* TASK B: Get the poster image, then populate the image view */
             if let posterPath = movie.posterPath {
