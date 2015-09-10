3b0ebeead33b7d8338658b1c184ec7bfd4f6b6f1
Jarrod Parkes <jarrod@udacity.com>
2015-09-10 12:29:18 -0500

    set keyboard auto correct to true to avoid weird hiding issues

---
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Jarrod Parkes on 1/29/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var defaultLabel: UILabel!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+
+}
